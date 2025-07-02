40d063cf60ccbc478ce258f79dc0acc141125c4e
juliasaboya <juliasaboyaengenharia@gmail.com>
2025-07-02 13:25:05 -0300

    Initial Commit

---
+++ deadlock-so/deadlock_soApp.swift
@@ -0,0 +1,17 @@
+//
+//  deadlock_soApp.swift
+//  deadlock-so
+//
+//  Created by Júlia Saboya on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct deadlock_soApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
